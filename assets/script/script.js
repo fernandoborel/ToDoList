279d38b560234955ec21592b79d7e22c469bb605
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-20 15:05:52 -0300

    commit adicionado

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,34 +1,28 @@
-let dados = [{
-    "marca": "Toyota",
-    "modelo": "Corolla",
-    "imagem": "https://w1.pngwing.com/pngs/1/539/png-transparent-2014-toyota-corolla-car-2016-toyota-corolla-2013-toyota-corolla-altis-toyota-corolla-altis-v-sedan-frontwheel-drive-latest.png"
-},
-{
-    "marca": "Fiat",
-    "modelo": "Palio",
-    "imagem": "https://img2.gratispng.com/20180611/zeu/kisspng-car-peugeot-rcz-fiat-palio-fiat-automobiles-jeep-cj-5b1f29a1e7a991.8980207115287689299489.jpg"
-},
-{
-    "marca": "Land Rover",
-    "modelo": "Evoque",
-    "imagem": "https://img2.gratispng.com/20180630/yyt/kisspng-2012-land-rover-range-rover-evoque-2015-land-rover-5b38069908f213.6606243615303983610367.jpg"
-},
-{
-    "marca": "Volkswagen",
-    "modelo": "T-Cross",
-    "imagem": "https://www.sanave.com.br/assets/uploads/nt_veiculos/91206-Imagem-topo.png?v=3"
-}];
+var endFilme = document.getElementById("endFilme");
 
-let infoDados = JSON.stringify(dados);
-let dadosAtualizados = JSON.parse(infoDados);
-let toyota = dadosAtualizados[0].modelo;
-console.log(toyota);
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  var legendaFilmes = document.getElementById("legenda").value;
+    if (filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito, legendaFilmes);
+    } else {
+        endFilme.innerHTML = "Endereço inválido!";
+    }
+  document.getElementById("filme").value = "";
+  document.getElementById("legenda").value = "";
+}
 
-dadosAtualizados.forEach(function() {
-    //document.body.innerHTML += "<p>"+dadosAtualizados[0].marca+"</p>";
-    document.body.innerHTML += "<p>"+toyota+"</p>";
-    //document.body.innerHTML += "<img src='https://w1.pngwing.com/pngs/1/539/png-transparent-2014-toyota-corolla-car-2016-toyota-corolla-2013-toyota-corolla-altis-toyota-corolla-altis-v-sedan-frontwheel-drive-latest.png'>"+dadosAtualizados[0]+"</>";
-});
+function listarFilmesNaTela(filme, legenda) {
+    var elementoFilmeFavorito = "<img src=" + filme + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    var elementoLegenda = "<figcaption>" + legenda + "</figcaption>";
+    var legendaInTela = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito + elementoLegenda;
+    var filmesAdicionados = [elementoListaFilmes];
+}
 
-const database = 'FeFFEr';
-
+function limparFilme() {
+  var limparFilme = document.getElementById("listaFilmes");
+  limparFilme.innerHTML = "";
+  endFilme.innerHTML = "";
+}
