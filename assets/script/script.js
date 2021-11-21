f1751be29c736cefe407673d06ef45ffebb9a57f
Fernando Borel <fernandomenezesborel@gmail.com>
2021-11-21 17:51:19 -0300

    finalizado por hj

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,31 @@
+var enterButtton = document.getElementById("enter");
+var input = document.getElementById("input");
+var ul = document.getElementById("ul");
+var item = document.getElementsByTagName("li");
 
+function inputLength() {
+    return input.value.length;
+}
 
+function createListElement() {
+    var li = document.createElement("li");
 
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+    function crossOut() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click", crossOut);
+
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
+}
+
+function deleteListItem() {
+    li.classList.add("delete");
+}
